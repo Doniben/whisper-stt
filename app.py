39ecd14bbcef9bb8b4098ffade2762ab55469b87
Doniben <doniben@esperanto.co>
2023-06-12 21:51:58 -0600

    just an example with Ashley interview

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import whisper
 
-model = whisper.load_model("base")
-result = model.transcribe("./Audio_Jeimmy.mp3")
+model = whisper.load_model("large")
+result = model.transcribe("Ashley12-06.mp4") # Agregar audio
 print(result["text"])
