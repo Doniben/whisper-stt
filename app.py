7ce39ace300beabe0d5dc34787975ad366f73ccb
Doniben <doniben@esperanto.co>
2023-06-23 19:17:14 -0600

    Updated code to send through email and adding to the stack

--- app.py
+++ app.py
@@ -1,5 +1,49 @@
 import whisper
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-model = whisper.load_model("large")
-result = model.transcribe("Ashley12-06.mp4") # Agregar audio
-print(result["text"])
+# Función para guardar el output en un archivo txt
+def guardar_output(texto, nombre_archivo):
+    with open(nombre_archivo, "w") as archivo:
+        archivo.write(texto)
+
+# Función para enviar un correo electrónico
+def enviar_correo(desde, para, asunto, cuerpo, servidor_smtp, puerto_smtp, usuario_smtp, contrasena_smtp):
+    mensaje = MIMEMultipart()
+    mensaje["From"] = desde
+    mensaje["To"] = para
+    mensaje["Subject"] = asunto
+    mensaje.attach(MIMEText(cuerpo, "plain"))
+
+    try:
+        servidor = smtplib.SMTP(servidor_smtp, puerto_smtp)
+        servidor.starttls()
+        servidor.login(usuario_smtp, contrasena_smtp)
+        servidor.send_message(mensaje)
+        servidor.quit()
+        print("Correo enviado correctamente.")
+    except Exception as e:
+        print("Error al enviar el correo:", str(e))
+
+def transcribir_archivos(archivos):
+    model = whisper.load_model("large")
+
+    for archivo in archivos:
+        result = model.transcribe(archivo)
+        texto_transcrito = result["text"]
+        print(texto_transcrito)
+
+        # Guardar el output en un archivo txt
+        nombre_archivo_output = archivo.split(".")[0] + ".txt"
+        guardar_output(texto_transcrito, nombre_archivo_output)
+
+        # Enviar correo electrónico
+        enviar_correo("doniben@esperanto.co", "tao_toons@yahoo.es", "Transcripción de audio " + nombre_archivo_output, texto_transcrito,
+                      "smtp.gmail.com", 587, "doniben@esperanto.co", "kmzwa8awaaAA")
+
+# Agregar varios archivos a una lista
+archivos_audio = ["ashley_prueba.mp4", "ashley_prueba2.mp4"]  # Agregar más archivos según sea necesario
+
+# Llamar a la función para transcribir los archivos
+transcribir_archivos(archivos_audio)
